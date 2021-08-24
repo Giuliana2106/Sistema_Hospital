5e6ae1fc2770772ac02d3ac5a993e9a9bad8e63c
Giuliana2106 <62736506+Giuliana2106@users.noreply.github.com>
2021-08-23 19:01:54 -0600

    Add files via upload

---
+++ templates_c/83368b4e0f7c528e17c5aae0d80abc39e04d42d0_0.file.Header.tpl.php
@@ -0,0 +1,44 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-08-23 19:33:22
+  from 'C:\xampp\htdocs\Sistema_Hospital\Templates\Cabeceras\Header.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_6123dbe22c3ad5_85084793',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '83368b4e0f7c528e17c5aae0d80abc39e04d42d0' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\Sistema_Hospital\\Templates\\Cabeceras\\Header.tpl',
+      1 => 1629739993,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6123dbe22c3ad5_85084793 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="Framework/Materialize/css/materialize.min.css"  media="screen,projection"/>
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>
+      <?php if ((isset($_smarty_tpl->tpl_vars['title']->value))) {?>
+        <?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+
+      <?php } else { ?>
+        Inicio de Sesión
+      <?php }?>
+      </title>
+    </head>
+
+    <body><?php }
+}
